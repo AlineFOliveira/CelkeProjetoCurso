d87d942d8b67923adebfc93f8308c1f187646cae
Aline <aliferroli18@gmail.com>
2024-02-26 08:55:28 -0300

    Change order of menu items

--- adm/app/adms/Models/AdmsEditPermission.php
+++ adm/app/adms/Models/AdmsEditPermission.php
@@ -4,7 +4,7 @@ namespace App\adms\Models;
 
 if (!defined('C8L6K7E')) {
     header("Location: /");
-    /* die('Erro: página não encontrada'); */
+    die("Erro: Página não encontrada<br>");
 }
 
 /**
@@ -24,9 +24,8 @@ class AdmsEditPermission
     /** @var int|string|null $id Recebe o id do registro */
     private int|string|null $id;
 
+    /** @var array|null $data Recebe as informações do formulário */
     private array|null $data;
-    private array|null $dataExitVal;
-
 
     /**
      * @return bool Retorna true quando executar o processo com sucesso e false quando houver erro
@@ -44,28 +43,33 @@ class AdmsEditPermission
         return $this->resultBd;
     }
 
+    /**
+     * Metodo recebe como parametro o ID que sera usado para verificar se tem o registro cadastrado no banco de dados
+     * @param integer $id
+     * @return void
+     */
     public function editPermission(int $id): void
     {
-        $this->id = (int) $id;
-        //var_dump($this->id);
-
+        $this->id = $id;
 
         $viewPermission = new \App\adms\Models\helper\AdmsRead();
         $viewPermission->fullRead(
             "SELECT lev_pag.id, lev_pag.permission
-            FROM adms_levels_pages lev_pag
-            INNER JOIN adms_access_levels AS lev ON lev.id=lev_pag.adms_access_level_id
-            LEFT JOIN adms_pages AS pag ON pag.id=adms_page_id
-            WHERE lev_pag.id=:id
-            AND lev.order_levels > :order_levels
-            AND (((SELECT permission FROM adms_levels_pages WHERE adms_page_id=lev_pag.adms_page_id AND adms_access_level_id={$_SESSION['adms_access_level_id']}) = 1) OR (publish = 1))
-            LIMIT :limit",
+                            FROM adms_levels_pages lev_pag
+                            INNER JOIN adms_access_levels AS lev ON lev.id=lev_pag.adms_access_level_id 
+                            LEFT JOIN adms_pages AS pag ON pag.id=lev_pag.adms_page_id 
+                            WHERE lev_pag.id =:id
+                            AND lev.order_levels >:order_levels
+                            AND (((SELECT permission 
+                            FROM adms_levels_pages 
+                            WHERE adms_page_id = lev_pag.adms_page_id 
+                            AND adms_access_level_id = {$_SESSION['adms_access_level_id']}) = 1) 
+                            OR (publish = 1))
+                            LIMIT :limit",
             "id={$this->id}&order_levels=" . $_SESSION['order_levels'] . "&limit=1"
         );
 
-        //var_dump($viewSit->getResult());
         $this->resultBd = $viewPermission->getResult();
-
         if ($this->resultBd) {
             $this->edit();
         } else {
@@ -74,19 +78,26 @@ class AdmsEditPermission
         }
     }
 
+    /**
+     * Metodo para alterar a permissao no banco de dados.
+     * Retorna TRUE quando editar no banco de dados.
+     * Retorna FALSE se houver algum erro.
+     * @return void
+     */
     private function edit(): void
     {
-        if ($this->resultBd[0]['permission'] == 1) {
+        if($this->resultBd[0]['permission'] == 1){
             $this->data['permission'] = 2;
-        } else {
+        }else{
             $this->data['permission'] = 1;
         }
         $this->data['modified'] = date("Y-m-d H:i:s");
+
         $upPermission = new \App\adms\Models\helper\AdmsUpdate();
         $upPermission->exeUpdate("adms_levels_pages", $this->data, "WHERE id=:id", "id={$this->id}");
 
         if ($upPermission->getResult()) {
-            $_SESSION['msg'] = "<p class='alert-success'>Permissão editada com sucesso!</p><br>";
+            $_SESSION['msg'] = "<p class='alert-success'>Permissão editada com sucesso!</p>";
             $this->result = true;
         } else {
             $_SESSION['msg'] = "<p class='alert-danger'>Erro: Permissão não editada com sucesso!</p>";
--- adm/app/adms/Models/AdmsOrderPageMenu.php
+++ adm/app/adms/Models/AdmsOrderPageMenu.php
@@ -4,11 +4,11 @@ namespace App\adms\Models;
 
 if (!defined('C8L6K7E')) {
     header("Location: /");
-    /* die('Erro: página não encontrada'); */
+    die("Erro: Página não encontrada<br>");
 }
 
 /**
- * Alterar a ordem do nivel de acesso no banco de dados
+ * Alterar ordem do item de menu no banco de dados
  *
  * @author Celke
  */
@@ -20,12 +20,15 @@ class AdmsOrderPageMenu
 
     /** @var array|null $resultBd Recebe os registros do banco de dados */
     private array|null $resultBd;
+
     /** @var array|null $resultBdPrev Recebe os registros do banco de dados */
     private array|null $resultBdPrev;
 
     /** @var int|string|null $id Recebe o id do registro */
     private int|string|null $id;
+    /** @var array|null $data Recebe os registros do banco de dados */
     private array|null $data;
+
     /**
      * @return bool Retorna true quando executar o processo com sucesso e false quando houver erro
      */
@@ -42,59 +45,78 @@ class AdmsOrderPageMenu
         return $this->resultBd;
     }
 
-    public function orderPageMenu(int $id): void
-    {
-        $this->id = (int)$id;
-
-        $viewPageMenu = new \App\adms\Models\helper\AdmsRead();
-        $viewPageMenu->fullRead(
-            "SELECT lev_pag.id, lev_pag.order_level_page, lev_pag.adms_access_level_id
-            FROM adms_levels_page lev_pag
-            INNER JOIN adms_access_levels AS lev ON lev.id=lev_pag.adms_access_level_id
-            WHERE lev_pag.id=:id AND lev.order_levels >=:order_levels
-            LIMIT :limit",
-            "id={$this->id}&order_levels=".$_SESSION['order_levels']."&limit=1"
-        );
-
-        // var_dump($viewUser->getResult());
-        $this->resultBd = $viewPageMenu->getResult();
-        if ($this->resultBd) {
-            $this->result = true;
-            $this->viewPrevPageMenu();
-        } else {
-            $_SESSION['msg'] = "<p class='alert-danger'>Erro: Nível de acesso não encontrado!</p>";
-            $this->result = false;
-        }
-    }
-
     /**
-     * Recupera a ordem do item de menu
-     *
+     * Metodo para alterar ordem do nivel de acesso
+     * Recebe o ID do nivel de acesso que sera usado como parametro na pesquisa
+     * Retorna FALSE se houver algum erro.
+     * @param integer $id
      * @return void
      */
-    private function viewPrevPageMenu(): void
+    public function orderPageMenu(int $id): void
     {
-        //var_dump($this->resultBd[0]['order_levels']);
-        $prevPageMenu = new \App\adms\Models\helper\AdmsRead();
-        $prevPageMenu->fullRead("SELECT id, order_level_page
-        FROM adms_levels_pages
-        WHERE order_levels_page <:order_levels_page
-        AND adms_access_level_id >:adms_access_level_id 
-        ORDER BY order_level_page DESC
-        LIMIT :limit", 
-        "order_levels={$this->resultBd[0]['order_levels']}&adms_access_level_id={$this->resultBd[0]['adms_access_level_id']}&limit=1");
-        $this->resultBdPrev = $prevPageMenu->getResult();
-        if ($this->resultBdPrev){
-            $this->result = true;
-            $this->editMoveDown();
-            $this->result = true;
+        $this->id = $id;
 
-        }else{
+        $viewAccessLevel = new \App\adms\Models\helper\AdmsRead();
+        $viewAccessLevel->fullRead("SELECT lev_pag.id, lev_pag.order_level_page, lev_pag.adms_access_level_id
+                        FROM adms_levels_pages lev_pag   
+                        INNER JOIN adms_access_levels AS lev ON lev.id=lev_pag.adms_access_level_id
+                        LEFT JOIN adms_pages AS pag ON pag.id=lev_pag.adms_page_id 
+                        WHERE lev_pag.id=:id 
+                        AND lev.order_levels >=:order_levels
+                        AND (((SELECT permission 
+                        FROM adms_levels_pages 
+                        WHERE adms_page_id = lev_pag.adms_page_id 
+                        AND adms_access_level_id = {$_SESSION['adms_access_level_id']}) = 1) 
+                        OR (publish = 1)) 
+                        LIMIT :limit", "id={$this->id}&order_levels=" . $_SESSION['order_levels'] . "&limit=1");
+
+        $this->resultBd = $viewAccessLevel->getResult();
+        if ($this->resultBd) {
+            $this->viewPrevPageMenu();
+        } else {
             $_SESSION['msg'] = "<p class='alert-danger'>Erro: Item de menu não encontrado!</p>";
             $this->result = false;
         }
     }
 
+    /**
+     * Metodo para recuperar o ordem do item de menu superior
+     * Retorna FALSE se houver algum erro.
+     * @return void
+     */
+    private function viewPrevPageMenu(): void
+    {
+        $prevPageMenu = new \App\adms\Models\helper\AdmsRead();
+        $prevPageMenu->fullRead(
+            "SELECT lev_pag.id, lev_pag.order_level_page 
+                        FROM adms_levels_pages AS lev_pag
+                        LEFT JOIN adms_pages AS pag ON pag.id=lev_pag.adms_page_id 
+                        WHERE lev_pag.order_level_page <:order_level_page
+                        AND lev_pag.adms_access_level_id =:adms_access_level_id
+                        AND (((SELECT permission 
+                        FROM adms_levels_pages 
+                        WHERE adms_page_id = lev_pag.adms_page_id 
+                        AND adms_access_level_id = {$_SESSION['adms_access_level_id']}) = 1) 
+                        OR (publish = 1)) 
+                        ORDER BY lev_pag.order_level_page DESC
+                        LIMIT :limit",
+            "order_level_page={$this->resultBd[0]['order_level_page']}&adms_access_level_id={$this->resultBd[0]['adms_access_level_id']}&limit=1"
+        );
+
+        $this->resultBdPrev = $prevPageMenu->getResult();
+        if ($this->resultBdPrev) {
+            $this->editMoveDown();
+        } else {
+            $_SESSION['msg'] = "<p class='alert-danger'>Erro: Item de menu não encontrado 3!</p>";
+            $this->result = false;
+        }
+    }
+
+    /**
+     * Metodo para alterar a ordem do menu superior para ser inferior
+     * Retorna FALSE se houver algum erro.
+     * @return void
+     */
     private function editMoveDown(): void
     {
         $this->data['order_level_page'] = $this->resultBd[0]['order_level_page'];
@@ -102,14 +124,20 @@ class AdmsOrderPageMenu
 
         $moveDown = new \App\adms\Models\helper\AdmsUpdate();
         $moveDown->exeUpdate("adms_levels_pages", $this->data, "WHERE id=:id", "id={$this->resultBdPrev[0]['id']}");
-        if($moveDown->getResult()){
+
+        if ($moveDown->getResult()) {
             $this->editMoveUp();
-        }else{
-            $_SESSION['msg'] = "<p class='alert-danger'>Erro: Ordem do item de menu não editada!</p>";
+        } else {
+            $_SESSION['msg'] = "<p class='alert-danger'>Erro: Ordem do item de menu não editado com sucesso!</p>";
             $this->result = false;
         }
     }
 
+    /**
+     * Metodo para alterar a ordem do item de menu inferior para ser superior
+     * Retorna FALSE se houver algum erro.
+     * @return void
+     */
     private function editMoveUp(): void
     {
         $this->data['order_level_page'] = $this->resultBdPrev[0]['order_level_page'];
@@ -118,13 +146,12 @@ class AdmsOrderPageMenu
         $moveUp = new \App\adms\Models\helper\AdmsUpdate();
         $moveUp->exeUpdate("adms_levels_pages", $this->data, "WHERE id=:id", "id={$this->resultBd[0]['id']}");
 
-        if($moveUp->getResult()){
-            $_SESSION['msg'] = "<p class='alert-success'> Ordem do item de menu editada com sucesso!</p>";
+        if ($moveUp->getResult()) {
+            $_SESSION['msg'] = "<p class='alert-success'>Ordem do item do menu editado com sucesso!</p>";
             $this->result = true;
-        }else{
-            $_SESSION['msg'] = "<p class='alert-danger'>Erro: Ordem do item de menu não editada!</p>";
+        } else {
+            $_SESSION['msg'] = "<p class='alert-danger'>Erro: Ordem do item do menu não editado com sucesso!</p>";
             $this->result = false;
         }
-    
     }
 }
